94cbac89f33bd59e180092f7293364fa18e5f34b
Madan Lal <madanparmar637@gmail.com>
2021-05-02 19:06:08 +0530

    command line arguments

--- cli1.js
+++ cli1.js
@@ -2,7 +2,17 @@
 
 "use strict";
 
-printHelp();
+// node cli.js --hello=world -c9
+// console.log(process.argv); // ['/usr/...','/use/...','--hello=world -c9']
+// console.log(process.argv.slice(2)); // ['--hello=world -c9']
+
+var args = require("minimist")(process.argv.slice(2),{
+  boolean: ['help'],
+  string: ['file']
+});
+console.log(args); // { _:[], hello:'world', c:9}
+
+// printHelp();
 
 // **************
 function printHelp(){
