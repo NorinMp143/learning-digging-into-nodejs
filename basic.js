06f976562ebca4ec8d48c25544bb1dd560b5b593
Madan Lal <madanparmar637@gmail.com>
2021-05-02 17:51:44 +0530

    initial + process standard

---
+++ basic.js
@@ -0,0 +1,10 @@
+// process.stdout.write('Hello');
+
+// console.log('Hello');
+
+// console.error('Oops');
+
+// different way of running node file
+// node basic.js 1>/dev/null
+// node basic.js 2>/dev/null
+// node basic.js 2>/dev/null 1>&2
