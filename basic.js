3426e425d5541ecebea03d9033a3e8ba3549c2c4
Madan Lal <madanparmar637@gmail.com>
2021-05-02 18:04:40 +0530

    setting up command line script

--- basic.js
+++ basic.js
@@ -1,10 +1,11 @@
-// process.stdout.write('Hello');
+process.stdout.write('Hello');
 
-// console.log('Hello');
+console.log('Hello');
 
-// console.error('Oops');
+console.error('Oops');
 
 // different way of running node file
 // node basic.js 1>/dev/null
 // node basic.js 2>/dev/null
 // node basic.js 2>/dev/null 1>&2
+
---
+++ cli1.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+"use strict";
+
+printHelp();
+
+// **************
+function printHelp(){
+  console.log("cli usage:");
+  console.log("  cli --help");
+  console.log("");
+  console.log(" --help          print this help");
+  console.log("");
+}
