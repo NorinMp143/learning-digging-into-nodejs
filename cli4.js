e89b6b411d10e494d9302585af79e6d8ebc85f27
Madan Lal <madanparmar637@gmail.com>
2021-05-08 23:31:14 +0530

    insert into database

--- cli4.js
+++ cli4.js
@@ -5,12 +5,8 @@
 const util = require('util');
 const path = require('path');
 const fs = require('fs');
-const zlib = require('zlib');
-
 
 const minimist = require('minimist');
-const { Transform } = require('stream');
-const CAF = require('caf');
 var sqlite3 = require('sqlite3');
 
 // ******************************
@@ -19,9 +15,10 @@ const DB_PATH  = path.join(__dirname, 'my.db');
 const DB_SQL_PATH = path.join(__dirname, 'mydb.sql');
 
 var args = minimist(process.argv.slice(2),{
-  boolean: ['help', 'in', 'out', 'compress', 'uncompress'],
-  string: ['file']
+  string: ['other']
 });
+
+main().catch(console.error);
 
 // ******************************
 
@@ -63,14 +60,31 @@ async function main(){
   if(otherId){
     let result = await insertSomething(otherId, something);
     if(result){
+      console.log('success');
       return;
     }
   }
   error('Oops');
 }
 
+async function insertSomething(otherId, something){
+  const result = await SQL3.run(
+    `
+      INSERT INTO
+        Something (otherId, data)
+      VALUES
+        (?,?)
+    `,
+    otherId, something
+  );
+  if(result && result.changes > 0){
+    return true;
+  }
+  return false;
+}
+
 async function insertOrLookupOther(other){
-  const result = await SQL3.get(
+  let result = await SQL3.get(
     `
       SELECT
         id
@@ -100,93 +114,10 @@ async function insertOrLookupOther(other){
   }
 }
 
-processFile = CAF(processFile);
-
-function streamComplete(stream){
-  return new Promise (function c(res){
-    stream.on("end",res);
-  })
-} 
-
-var BASH_PATH = path.resolve( process.env.BASE_PATH || __dirname)
-
-var OUTFILE = path.join(BASH_PATH, 'out.txt');
-
-if(args.help){
-  printHelp();
-}
-else if(args.in || args._.includes('-')){
-  const timeoutToken = CAF.timeout(3, "Timeout!");
-  processFile(timeoutToken,process.stdin)
-  .catch(error);
-}
-else if(args.file){
-  const stream = fs.createReadStream(path.join(BASH_PATH,args.file));
-  const timeoutToken = CAF.timeout(3, "Timeout!");
-  processFile(timeoutToken,stream)
-  .then((data)=>{
-    console.log('Complete!');
-  })
-  .catch(error);
-}
-else{
-  error('Incorrect usage',true)
-}
-
-// **************
-
-function *processFile (signal,inStream){
-  let outStream = inStream;
-
-  if(args.uncompress){
-    const gunzipStream = zlib.createGunzip();
-    outStream = outStream.pipe(gunzipStream);
-  }
-
-  const upperStream = new Transform({
-    transform(chunk, enc, cb){
-      this.push(chunk.toString().toUpperCase());
-      cb();
-    }
-  });
-  outStream = outStream.pipe(upperStream);
-  if(args.compress){
-    const gzipStream = zlib.createGzip();
-    outStream = outStream.pipe(gzipStream);
-    OUTFILE = `${OUTFILE}.gz`;
-  }
-  let targetStream;
-  if(args.out){
-    targetStream = process.stdout;
-  }
-  else{
-     targetStream = fs.createWriteStream(OUTFILE);
-  }
-  outStream.pipe(targetStream);
-  signal.pr.catch(function f(){
-    outStream.unpipe(targetStream);
-    outStream.destroy();
-  })
-  yield streamComplete(outStream)
-}
-
 function error(msg, includeHelp= false){
   console.log(msg);
   if(includeHelp){
     console.log('');
     printHelp();
   }
-}
-
-function printHelp(){
-  console.log("cli3 usage:");
-  console.log("  cli3.js --file={FILENAME}");
-  console.log("");
-  console.log(" --help          print this help");
-  console.log(" --file          process the file");
-  console.log(" --in, -         process stdin");
-  console.log(" --out           print the output");
-  console.log(" --compress      gzip the output");
-  console.log(" --uncompress    un-zip the input");
-  console.log("");
 }
